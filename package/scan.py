544cf192aadb42df03ddf46b91bd80833e62330a
Andreas Albert <andreas.albert@cern.ch>
2021-01-12 15:27:40 +0100

    Add vector scan

--- package/scan.py
+++ package/scan.py
@@ -1,9 +1,29 @@
 from dataclasses import dataclass
+from enum import Enum
 import numpy as np
 import abc
 
 PI = np.pi
 
+class Quarks(Enum):
+    up=0.0024
+    down=0.0048
+    strange=0.104
+    charm=1.27
+    bottom=4.2
+    top=171.2
+
+class Leptons(Enum):
+    electron=0.000511
+    muon=0.105658
+    tau=1.77682
+
+
+def alpha(x, y):
+    """
+    Convenience function that implements part of the width formulae.
+    """
+    return 1 + 2 * x**2 / y**2
 
 def beta(x, y):
     """
@@ -65,3 +85,52 @@ class DMScalarModelScan(DMModelScan):
             gamma,
             0
         )
+
+
+
+@dataclass
+class DMVectorModelScan(DMModelScan):
+    '''
+    Specific implementation of a parameter scan
+    for a vector mediator.
+    '''
+    _coupling: str = 'vector'
+
+    def mediator_total_width(self):
+        return self.mediator_partial_width_quarks() + self.mediator_partial_width_dm() + self.mediator_partial_width_leptons()
+        pass
+
+    def mediator_partial_width_quarks(self):
+        width = 0
+        for mq in Quarks:
+            iwidth = 3 * self.gq**2 * self.mmed / \
+                    (12 * PI) * alpha(mq, self.mmed) * beta(mq, self.mmed)
+            
+            # Only add width for mq < mmed
+            width = np.where(
+                mq < self.mmed * 0.5,
+                width + iwidth,
+                width
+            )
+        return width
+
+    def mediator_partial_width_dm(self):
+        width = self.gdm**2 * self.mmed / (12 * PI) * alpha(self.mdm, self.mmed) * beta(self.mdm, self.mmed)
+        return np.where(
+            self.mdm < self.mmed * 0.5,
+            width,
+            0
+        )
+
+    def mediator_partial_width_leptons(self):
+        width = 0
+        for ml in Leptons:
+            iwidth = self.gl**2 * self.mmed / (12*PI) * alpha(ml, self.mmed) / beta(ml, self.mmed)
+            
+            # Only add width for ml < mmed
+            width = np.where(
+                ml < self.mmed * 0.5,
+                width + iwidth,
+                width
+            )
+    return width
