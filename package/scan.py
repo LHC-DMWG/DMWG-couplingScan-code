481c7f34f045b32310c6ac1b8300ad8c90cd93da
Katherine Pachal <katepachal@gmail.com>
2021-12-21 11:00:55 -0800

    beginning move to piecewise

--- package/scan.py
+++ package/scan.py
@@ -2,6 +2,15 @@ from dataclasses import dataclass
 from enum import Enum
 import numpy as np
 import abc
+import imp
+import scipy.integrate as integrate
+
+# Check if lhapdf was available at compile time. 
+try:
+    imp.find_module('package')
+    hasLHAPDF = True
+except:
+    hasLHAPDF = False
 
 PI = np.pi
 
@@ -29,11 +38,18 @@ def beta(x, y):
     """
     Convenience function that implements part of the width formulae.
     """
-    x = np.complex(x,0)
-    y = np.complex(y,0)
-    return np.sqrt(1 - 4 * x**2 / y**2)
+    # Not sure why these are here?
+    #x = np.complex(x,0)
+    #y = np.complex(y,0)
+    print(x, y)
+    print(4 * x**2/y**2)
+    print(1 - 4 * x**2 / y**2)
+    #return np.sqrt(1 - 4 * x**2 / y**2)
+    val = np.sqrt(1 - 4 * x**2 / y**2)
+    print(val)
+    return val
 
-def limit_x1(x2,pid,gamma,M,mDM) :
+def limit_x1(x2,pid,gamma,M,mDM,ECM) :
     """
     Integration limits for x1, x2 space
     """
@@ -43,7 +59,7 @@ def limit_x1(x2,pid,gamma,M,mDM) :
     return [lower_lim, 1]
 
 
-def limit_x2(pid,gamma,M,mDM) :
+def limit_x2(pid,gamma,M,mDM,ECM) :
     """
     Integration limits for x1, x2 space
     """    
@@ -64,6 +80,16 @@ class DMModelScan(abc.ABC):
     gl: float
     _coupling: str
 
+    ECM: float = 13000.**2
+
+    # Initialise handler for lhapdfwrap if compiled
+    # with lhapdf available.
+    # To determine reliably from here, check for generated file.
+    _wrapper = None
+    if (hasLHAPDF) :
+        import lhapdfwrap as pdfwrap
+        _wrapper = pdfwrap.IntegrandHandler("NNPDF30_nlo_as_0118", ECM)
+
     @abc.abstractmethod
     def mediator_total_width(self):
         pass
@@ -76,10 +102,27 @@ class DMModelScan(abc.ABC):
     def mediator_partial_width_dm(self):
         pass
 
-    # and so on for more partial width functions,
-    # propagator terms, whatever ...
-    def propagator(self):
-        pass
+    # "Relative" in function names from here on
+    # indicates that these are not full cross sections
+    # but do correctly define ratios of cross sections
+    # calculated using the same functions.
+
+    # May want to move these since some are signature dependent
+    # I.e. are there equivalents to these for scalar/pseudoscalar?
+    # @abc.abstractmethod
+    # def propagator_monox_relative(self) :
+    #     pass
+
+    # @abc.abstractmethod
+    # def hadron_level_xsec_monox_relative(self) :
+    #     pass
+
+    # @abc.abstractmethod
+    # def parton_level_xsec_monox_relative(self) :
+    #     pass
+
+    # TODO:
+    # Add for resonances!
 
 
 @dataclass
@@ -189,29 +232,30 @@ class DMVectorModelScan(DMModelScan):
         '''
         On-shell width for mediator -> q q.
         '''
+
         width = 0
         for mq in Quarks:
-            iwidth = 3 * self.gq**2 * self.mmed / \
-                    (12 * PI) * alpha(mq.value, self.mmed) * beta(mq.value, self.mmed)
-
-            # Only add width for mq < mmed
-            width = np.where(
-                mq.value < self.mmed * 0.5,
-                width + iwidth,
-                width
+            # Only calculate when mq < 0.5 mmed, or we get errors.
+            iwidth = np.piecewise(
+                self.mmed,
+                [mq.value < self.mmed * 0.5],
+                [lambda x : 3 * self.gq**2 * x / (12 * PI) * alpha(mq.value, x) * beta(mq.value, x), 0]
             )
+            width += iwidth
+
+
         return width
 
     def mediator_partial_width_dm(self):
         '''
         On-shell width for mediator -> DM DM.
         '''
-        width = self.gdm**2 * self.mmed / (12 * PI) * alpha(self.mdm, self.mmed) * beta(self.mdm, self.mmed)
-        return np.where(
-            self.mdm < self.mmed * 0.5,
-            width,
-            0
+        width = np.piecewise(
+            self.mmed,
+            [self.mdm < self.mmed * 0.5],
+            [lambda x : self.gdm**2 * x / (12 * PI) * alpha(self.mdm, x) * beta(self.mdm, x), 0]
         )
+        return width
 
     def mediator_partial_width_leptons(self):
         '''
@@ -231,6 +275,43 @@ class DMVectorModelScan(DMModelScan):
                 width
             )
         return width
+
+    # Vector
+    def propagator_monox_relative(self) :
+        print("Start")
+        print("Mediator masses:",self.mmed)
+        print("DM masses:",self.mdm
+        )
+        gamma = self.mediator_total_width()
+        print("Got gammas")
+        arctan_factor = PI/2.0 + np.arctan((self.mmed**2 - 4.*self.mdm**2)/(self.mmed*gamma))
+        sigma = self.gq**2 * self.gdm**2 * arctan_factor/(self.mmed*gamma)
+        return sigma
+
+    def hadron_level_xsec_monox_relative(self) :
+        return 1  
+
+
+    # In case of future relevance: parton level relative xsec
+    def parton_level_xsec_monox_relative(self) :
+
+        gamma = self.mediator_total_width()
+        intpoints = [self.mmed,self.mmed**2-gamma,self.mmed**2,self.mmed**2+gamma]
+
+        # Integrate is adaptive and fundamentally
+        # doesn't work with broadcasting.
+        # So for this function we are going to have to 
+        # actually do the values one at a time.
+        xsecs = []
+        if type(self.mmed) is np.ndarray or type(self.mdm) is np.ndarray :
+          for mmed_i, mdm_i, gamma_i, points_i in zip(self.mmed, self.mdm, gamma, intpoints) :
+            integral = integrate.quad(self._wrapper.integrand_parton_vector,4.*mdm_i**2,self.ECM,args=(gamma_i,mmed_i,mdm_i),points=points_i,limit=500)
+            xsecs.append(self.gq**2 * self.gdm**2 * integral[0])
+        else :
+          integral = integrate.quad(self._wrapper.integrand_parton_vector,4.*self.mdm**2,self.ECM,args=(gamma,self.mmed,self.mdm),points=intpoints,limit=500)
+          xsecs.append(self.gq**2 * self.gdm**2 * integral[0])
+        
+        return np.array(xsecs)
     
 @dataclass
 class DMAxialModelScan(DMModelScan):
--- test/simple_test.py
+++ test/simple_test.py
@@ -2,27 +2,65 @@ from package.scan import *
 
 # Example one: mostly constant parameters, scan in 1d
 # numpy broadcasting does its magic
-scan1 = DMScalarModelScan(
-mmed=1000,
+# scan1 = DMScalarModelScan(
+# mmed=1000,
+# mdm=np.array([1,10,50,100,150,200,250,300,350,400,450]),
+# gq=0.25,
+# gdm=1.0,
+# gl=0.0,
+# )
+# print("Scan 1:")
+# print(scan1.mediator_partial_width_dm() / scan1.mmed)
+
+# # Example two: multiple variables scanned
+# # numpy broadcasting still handles this
+# scan2 = DMScalarModelScan(
+# mmed=3*np.array([1,10,50,100,150,200,250,300,350,400,450]),
+# mdm=np.array([1,10,50,100,150,200,250,300,350,400,450]),
+# gq=0.25,
+# gdm=1.0,
+# gl=0.0,
+# )
+# print("Scan 2:")
+# print(scan2.mediator_partial_width_dm() / scan2.mmed)
+
+# Example three: propagators, arrays
+scan3 = DMVectorModelScan(mmed=3*np.array([1,10,50,100,150,200,250,300,350,400,450]),
 mdm=np.array([1,10,50,100,150,200,250,300,350,400,450]),
 gq=0.25,
 gdm=1.0,
 gl=0.0,
 )
-print(scan1.mediator_partial_width_dm() / scan1.mmed)
+print("Scan 3:")
+print(scan3.mediator_partial_width_quarks())
+#print(scan3.propagator_monox_relative())
 
-# Example two: multiple variables scanned
-# numpy broadcasting still handles this
-scan2 = DMScalarModelScan(
-mmed=3*np.array([1,10,50,100,150,200,250,300,350,400,450]),
-mdm=np.array([1,10,50,100,150,200,250,300,350,400,450]),
-gq=0.25,
-gdm=1.0,
-gl=0.0,
-)
-print(scan2.mediator_partial_width_dm() / scan2.mmed)
+# Example four: parton-level, no arrays
+# scan4 = DMVectorModelScan(mmed=1000,
+# mdm=400,
+# gq=0.25,
+# gdm=1.0,
+# gl=0.0,
+# )
+# print("Scan 4:")
+# print(scan4.parton_level_xsec_monox_relative())
+# print("done")
 
-# Example n: Try to get serious conversion items.
+# # Example four and a half: propagators, arrays
+# scan4p5 = DMVectorModelScan(mmed=3*np.array([1,10,50,100,150,200,250,300,350,400,450]),
+# mdm=np.array([1,10,50,100,150,200,250,300,350,400,450]),
+# gq=0.25,
+# gdm=1.0,
+# gl=0.0,
+# )
+# print("Scan 4.5:")
+# print(scan4p5.parton_level_xsec_monox_relative())
+
+# Example four: parton level xsec
+
+
+# Example five: hadron level xsec
+
 
 # Example three: Inconsistent array sizes
 # broadcasting fails
@@ -32,11 +70,11 @@ print(scan2.mediator_partial_width_dm() / scan2.mmed)
 # Would be more natural for user's expectation.
 # Or else make it very clear what they're getting.
 # I guess this is better because it allows uneven grids.
-scan3 = DMScalarModelScan(
-mmed=3*np.array([1,10]),
-mdm=np.array([1,10,50,100,150,200,250,300,350,400,450]),
-gq=0.25,
-gdm=1.0,
-gl=0.0,
-)
-print(scan3.mediator_partial_width_dm() / scan3.mmed)
+# scan3 = DMScalarModelScan(
+# mmed=3*np.array([1,10]),
+# mdm=np.array([1,10,50,100,150,200,250,300,350,400,450]),
+# gq=0.25,
+# gdm=1.0,
+# gl=0.0,
+# )
+# print(scan3.mediator_partial_width_dm() / scan3.mmed)
